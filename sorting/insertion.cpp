57f3e16143ebadce0e410873676bb5bcb59cc47a
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2020-08-13 00:56:22 +0530

    Create insertion.cpp

---
+++ sorting/insertion.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int *a;
+  a=new int[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
+  for(int i=1;i<n;i++)
+  {
+    int j=0;
+    int temp=a[i];
+    while(temp<a[i] && j>=0)
+    {
+      a[i+1] = a[i];
+      j++;
+     }
+     a[j+1] = temp;
+  }
+  display(a);
+  return 0;
+}
