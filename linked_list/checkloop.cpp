5fa3f5f9fb91d7cd38a13f189b43af52b63c062c
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2020-08-17 21:57:25 +0530

    Create checkloop.cpp

---
+++ linked_list/checkloop.cpp
@@ -0,0 +1,21 @@
+//extension fucntion to check if linked list is a loop or not//
+void loop()
+{
+  struct node *p,*q;  
+  p=head;
+  q=head;
+  while(p!=q)
+    {
+    p=p->next;
+    q=q->next;
+    if(q->next!=NULL)
+      {
+        p=p->next;
+      }
+     }
+  if(p->data==q->data)
+      {
+      cout<<"LOOP";
+      }
+   else cout<<"NULL";
+}
