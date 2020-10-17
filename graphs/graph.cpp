d1c7adc24c7e77b168bc7abcd8ccca0691a42767
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2020-10-17 13:06:43 +0530

    added macros

--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -1,2 +1,15 @@
 #inlcude<bits/stdc++.h>
 using namespace std;
+#define ll  long long
+#define ull usigned long long 
+#define vecu vector<ull> 
+#define vec vector<ll>
+#define vec1 vector<vector ll>>
+#define arr array<ll,1000000> 
+#define str string
+#define set set<ll>
+#define mset multiset<ll>
+#define pair pair<ll,ll>
+#define pb push_back
+#define sort(a) sort(a.begin(),a.end())
+#define sort1(a) sort(a.begin(),a.end(),greater<ll> ())
